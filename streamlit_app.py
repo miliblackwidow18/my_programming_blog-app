61fbee67e23a99b808821879b6f0e66c6c97b056
miliblackwidow18 <milidayalushe@gmail.com>
2024-11-25 02:54:12 +0000

    título en español y botones dinámicos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 El blog de una principiante en Python")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+calificación = st.slider("¿Cómo calificarías tu experiencia aprendiendo a programar en Python?")
+
+st.write(f"la calificación seleccionada es: {calificación} ⭐")
